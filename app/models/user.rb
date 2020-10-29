613ffcdb69363fc9fdbd2ac7e9cbfae38c645011
thanhlong-dev <thanhlongdn.it@gmail.com>
2020-10-29 16:21:19 +0700

    Chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,13 @@ class User < ApplicationRecord
     length: {minimum: Settings.model_user.minimum_password.to_i}
 
   has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   root to: "static_pages#home"
 
   get "static_pages/home"
-  get "static_pages/help"
+  get "help", to: "static_pages#help"
 
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users, only: [:new, :show, :create]
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  resources :sessions, only: [:new, :show, :create]
 end
