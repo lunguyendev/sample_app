373bebdaeb290302d14a97a4173c1f7835b648e7
thanhlong-dev <thanhlongdn.it@gmail.com>
2020-10-29 16:21:19 +0700

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: "static_pages#home"
+
   get "static_pages/home"
   get "static_pages/help"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: [:new, :show, :create]
 end
